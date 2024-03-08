db1eb11a832f4043d20211bfaf97273716f5d786
Nicolas <nicolaspantojadi@gmail.com>
2024-03-08 12:46:39 -0500

    feat: new logo in lazy

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -99,4 +99,50 @@ return {
       })
     end,
   },
+
+  --bufferline
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S_Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+
+  -- animations
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
+
+  --- logo
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
+███╗   ██╗██╗ ██████╗ ██████╗ ███╗   ███╗███╗   ███╗██╗████████╗        
+████╗  ██║██║██╔════╝██╔═══██╗████╗ ████║████╗ ████║██║╚══██╔══╝        
+██╔██╗ ██║██║██║     ██║   ██║██╔████╔██║██╔████╔██║██║   ██║           
+██║╚██╗██║██║██║     ██║   ██║██║╚██╔╝██║██║╚██╔╝██║██║   ██║           
+██║ ╚████║██║╚██████╗╚██████╔╝██║ ╚═╝ ██║██║ ╚═╝ ██║██║   ██║           
+╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝╚═╝   ╚═╝           
+                                                                        
+      ]]
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
 }
