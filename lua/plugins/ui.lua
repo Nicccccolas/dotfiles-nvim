041d617f0fa2cd2d71f961e399524c9e35a37546
Nicolas <nicolaspantojadi@gmail.com>
2024-03-08 11:18:24 -0500

    feat: name of incline in tabs

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,6 @@
 return {
+
+  -- Messages, cmdline and the popumenu
   {
     "folke/noice.nvim",
     opts = function(_, opts)
@@ -6,14 +8,95 @@ return {
         filter = { event = "notify", find = "No information available" },
         opts = { skip = true },
       })
+      local focused = true
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = function()
+          focused = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("FocusLost", {
+        callback = function()
+          focused = false
+        end,
+      })
+      table.insert(opts.routes, 1, {
+        filter = {
+          cond = function()
+            return not focused
+          end,
+        },
+        view = "notify_send",
+        opts = { stop = false },
+      })
+
+      opts.commands = {
+        all = {
+          -- options for the message history that you get with ":Noice"
+          view = "split",
+          opts = { enter = true, format = "details" },
+          filter = {},
+        },
+      }
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function()
+          vim.schedule(function()
+            require("noice.text.markdown").keys(event.buf)
+          end)
+        end,
+      })
 
       opts.presets.lsp_doc_border = true
     end,
   },
+
   {
     "rcarriga/nvim-notify",
     opts = {
-      timeout = 10000,
+      timeout = 6000,
     },
+  },
+
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "solarized_dark",
+      },
+    },
+  },
+
+  -- filename
+  {
+    "b0o/incline.nvim",
+    dependencies = { "craftzdog/solarized-osaka.nvim" },
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local colors = require("solarized-osaka.colors").setup()
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+            InclineNormalNc = { guifg = colors.violet500, guibg = colors.base03 },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+]" .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_colors(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
   },
 }
